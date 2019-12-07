8d0f34ade697efeaa705fd823165e7f762e56c57
YogeeshVD <kekkaryogeesh@gmail.com>
2019-12-07 15:36:45 +0530

    Web module

--- WebContent/js/script.js
+++ WebContent/js/script.js
@@ -1,1 +1,46 @@
 // Include form validation functions here
+function validate() {
+		var titl = document.forms["form1"]["txtTitle"].value;
+		var bxoffice = document.forms["form1"]["txtPrice"].value;
+		var dol= document.forms["form1"]["txtDol"].value;
+		
+		if(titl.trim()=="") {
+			alert(" Title is required.");
+			return false;
+			
+		} else if( bxoffice.trim()=="") {
+			alert(" Box is required.");
+			return false;
+			
+		}else if(dol.trim()=="") {
+			alert(" Date of Launch is required");
+			return false;
+		}else {
+		 true;
+		}
+		
+		if(/[^0-9\-\\/\/]/.test(dol)) {
+			alert("Date is to be a number");
+			return false;
+		} else {
+			true;
+		}
+		
+		if(/[^0-9]/.test(bxoffice)) {
+			alert("Box is to be a number");
+			return false;
+		} else {
+			true;
+		}
+		
+		
+
+		if(titl.length <2 || nam.length >100) {
+			alert("Title should have 2-100 characters");
+			return false; 
+		} else {
+			true;
+		}
+	}
+
+	
